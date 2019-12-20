48a311354b93948e2af38be40e2fe50c713a5a57
Max Coyle <55137324@ad.mmu.ac.uk>
2019-12-20 16:14:06 +0000

    import code from old repo

---
+++ HarryPotterLogic/HarryPotterService.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace HarryPotterLogic
+{
+    public class HarryPotterService
+    {
+        public List<BookSet> BookSets { get; }
+
+        public HarryPotterService()
+        {
+            BookSets = new List<BookSet>();
+        }
+
+        public void AddBookToBasket(string bookName)
+        {
+            if (AddBookToExistingBookset(bookName)) return;
+            AddBookToNewBookSet(bookName);
+        }
+
+        private bool AddBookToExistingBookset(string bookName)
+        {
+            var bookAddedToExistingBookset = false;
+
+            foreach (var bookSet in BookSets)
+            {
+                if (bookSet.Books.Contains(bookName)) continue;
+                bookSet.Books.Add(bookName);
+                bookAddedToExistingBookset = true;
+                break;
+            }
+
+            return bookAddedToExistingBookset;
+        }
+
+        private void AddBookToNewBookSet(string bookName)
+        {
+            var newBookSet = new BookSet();
+            newBookSet.Books.Add(bookName);
+            BookSets.Add(newBookSet);
+        }
+
+        public decimal BasketTotal => GetBasketTotal();
+
+        private decimal GetBasketTotal()
+        {
+            decimal basketTotal = 0;
+
+            foreach (var bookSet in BookSets)
+            {
+                basketTotal = basketTotal + bookSet.Total;
+            }
+
+            return basketTotal;
+        }
+    }
+}
---
+++ HarryPotterTests/HarryPotterTests.cs
@@ -0,0 +1,210 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace HarryPotterTests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void OneBookShouldHaveNoDiscount()
+        {
+            // Arrange
+            var harryPotterService = new HarryPotterLogic.HarryPotterService();
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+
+            // Act
+            var response = harryPotterService.BasketTotal;
+
+            // Assert
+            response.Should().Be(8);
+        }
+
+        [Fact]
+        public void TwoDifferentBooksShouldHaveFivePercentDiscount()
+        {
+            // Arrange
+            var harryPotterService = new HarryPotterLogic.HarryPotterService();
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheChamberOfSecrets);
+
+            // Act
+            var response = harryPotterService.BasketTotal;
+
+            // Assert
+            response.Should().Be((decimal)15.2);
+        }
+
+        [Fact]
+        public void ThreeDifferentBooksShouldHaveTenPercentDiscount()
+        {
+            // Arrange
+            var harryPotterService = new HarryPotterLogic.HarryPotterService();
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheChamberOfSecrets);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePrisonerOfAzkaban);
+
+            // Act
+            var response = harryPotterService.BasketTotal;
+
+            // Assert
+            response.Should().Be((decimal)21.6);
+        }
+
+        [Fact]
+        public void FourDifferentBooksShouldHaveFifteenPercentDiscount()
+        {
+            // Arrange
+            var harryPotterService = new HarryPotterLogic.HarryPotterService();
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheChamberOfSecrets);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePrisonerOfAzkaban);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheGobletOfFire);
+
+            // Act
+            var response = harryPotterService.BasketTotal;
+
+            // Assert
+            response.Should().Be((decimal)27.2);
+        }
+
+        [Fact]
+        public void FiveDifferentBooksShouldHaveTwentyPercentDiscount()
+        {
+            // Arrange
+            var harryPotterService = new HarryPotterLogic.HarryPotterService();
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheChamberOfSecrets);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePrisonerOfAzkaban);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheGobletOfFire);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheOrderOfThePhoenix);
+
+            // Act
+            var response = harryPotterService.BasketTotal;
+
+            // Assert
+            response.Should().Be(32);
+        }
+
+        [Fact]
+        public void SixDifferentBooksShouldHaveTwentyFivePercentDiscount()
+        {
+            // Arrange
+            var harryPotterService = new HarryPotterLogic.HarryPotterService();
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheChamberOfSecrets);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePrisonerOfAzkaban);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheGobletOfFire);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheOrderOfThePhoenix);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheHalfBloodPrince);
+
+            // Act
+            var response = harryPotterService.BasketTotal;
+
+            // Assert
+            response.Should().Be(36);
+        }
+
+        [Fact]
+        public void SevenDifferentBooksShouldHaveThirtyPercentDiscount()
+        {
+            // Arrange
+            var harryPotterService = new HarryPotterLogic.HarryPotterService();
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheChamberOfSecrets);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePrisonerOfAzkaban);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheGobletOfFire);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheOrderOfThePhoenix);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheHalfBloodPrince);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheDeathlyHallows);
+
+            // Act
+            var response = harryPotterService.BasketTotal;
+
+            // Assert
+            response.Should().Be((decimal)39.2);
+        }
+
+        [Fact]
+        public void TwoCopiesOfSameBookHaveNoDiscount()
+        {
+            // Arrange
+            var harryPotterService = new HarryPotterLogic.HarryPotterService();
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+
+            // Act
+            var response = harryPotterService.BasketTotal;
+
+            // Assert
+            response.Should().Be(16);
+        }
+
+        [Fact]
+        public void KataTest1()
+        {
+            // Arrange
+            var harryPotterService = new HarryPotterLogic.HarryPotterService();
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheChamberOfSecrets);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheChamberOfSecrets);
+
+            // Act
+            var response = harryPotterService.BasketTotal;
+
+            // Assert
+            response.Should().Be((decimal)38.4);
+        }
+
+        [Fact]
+        public void KataTest2()
+        {
+            // Arrange
+            var harryPotterService = new HarryPotterLogic.HarryPotterService();
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheChamberOfSecrets);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheChamberOfSecrets);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePrisonerOfAzkaban);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheGobletOfFire);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheGobletOfFire);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheGobletOfFire);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheGobletOfFire);
+
+            // Act
+            var response = harryPotterService.BasketTotal;
+
+            // Assert
+            response.Should().Be((decimal)72);
+        }
+
+        [Fact]
+        public void KataTest3()
+        {
+            // Arrange
+            var harryPotterService = new HarryPotterLogic.HarryPotterService();
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePhilosophersStone);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheChamberOfSecrets);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheChamberOfSecrets);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.ThePrisonerOfAzkaban);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheGobletOfFire);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheGobletOfFire);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheGobletOfFire);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheGobletOfFire);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheOrderOfThePhoenix);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheHalfBloodPrince);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheHalfBloodPrince);
+            harryPotterService.AddBookToBasket(HarryPotterLogic.HarryPotterBooks.TheDeathlyHallows);
+
+            // Act
+            var response = harryPotterService.BasketTotal;
+
+            // Assert
+            response.Should().Be((decimal)89.6);
+        }
+    }
+}
